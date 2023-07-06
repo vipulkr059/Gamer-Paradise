820f26bfa8debfa46898e820c5dadeb57a1b44b3
vipulkr059 <vipulkr059@gmail.com>
2023-07-06 23:59:17 +0530

    NavBar Layout

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,11 @@
-import React from "react";
+import { HStack, Image, Text } from "@chakra-ui/react";
+import logo from "../assets/logo.webp";
 
 export const NavBar = () => {
-  return <div>NavBar</div>;
+  return (
+    <HStack>
+      <Image src={logo} boxSize="60px" />
+      <Text>NavBar</Text>
+    </HStack>
+  );
 };
